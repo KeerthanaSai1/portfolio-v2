071c943b2cb603cdb02264a23a604e0ec8d4588f
Saikeerthana <saijampani453@gmail.com>
2026-04-15 17:37:33 +0530

    Remove useRef Import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Portfolio = () => {
   const [isDark, setIsDark] = useState(true);
